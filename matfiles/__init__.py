d95e987d358513181ef2a9b83e5709845187f4ab
Austin Crabtree <crabbyinatree@protonmail.com>
2021-03-19 22:14:10 -0400

    moved and setup module

---
+++ matfiles/__init__.py
@@ -0,0 +1,1 @@
+from .loadmat import loadmat
--- matfiles/loadmat.py
+++ matfiles/loadmat.py
@@ -2,12 +2,43 @@
 #  the data, but if < 7.3 uses scipy.io.loadmat function
 
 import h5py
+import numpy as np
 
 from pathlib import Path
-from typing import Union, Optional, Dict, BinaryIO
+from typing import Union, Optional, Dict, List
 
 
-def loadmat(filename: Optional[str, Path]) -> Optional[Dict]:
+def _parse_group(group: Union[h5py._hl.group.Group, h5py._hl.dataset.Dataset], skip: List = ['#refs#']) -> Union[Dict, np.ndarray]:
+    """
+    parse members of the hdf5 group and add to contents to return
+    Parameters
+    ----------
+    group : Union[h5py._hl.group.Group, h5py._hl.dataset.Dataset]
+        either a HDF5 group or dataset
+    skip : List
+        list of groups or datasets to ignore while parsing. In the case of this module we will always skip #refs#
+
+    Returns
+    -------
+    contents : Union[Dict, np.ndarray]
+        contents of the current group can either be a dictionary or array
+    """
+    if isinstance(group, h5py._hl.group.Group):
+        # Since this is a group I will loop through its children
+        contents = {}
+        groups = group.keys()
+        for group_name in groups:
+            # name = group_name.split('/')[-1]
+            if group_name in skip:
+                continue
+            _group = group[group_name]
+            contents[group_name] = _parse_group(_group)
+    elif isinstance(group, h5py._hl.dataset.Dataset):
+        contents = group[()]
+    return contents
+
+
+def loadmat(filename: Union[str, Path]) -> Optional[Dict]:
     """
     loads a v7.3 mat file and returns a dictionary with the contents
 
@@ -35,17 +66,7 @@ def loadmat(filename: Optional[str, Path]) -> Optional[Dict]:
         # contents in the other groups.
 
         # TODO grab all the groups and start building the keys in my dictionary
-        groups = matfile.keys()
-        # Try to remove the #refs# groups so we don't interact with it. If it doesn't exist then just move on
-        try:
-            groups.remove('#refs#')
-        except ValueError:
-            pass
-        for group in groups:
-            set = matfile['group']
-            if isinstance(set, h5py._hl.group.Group):
-                print(f'Is group {set.name}')
-
+        contents = _parse_group(matfile)
         return contents
     else:
         raise TypeError('File provided is not an HDF5 file. Please make sure you providing a v7.3 mat file.')
